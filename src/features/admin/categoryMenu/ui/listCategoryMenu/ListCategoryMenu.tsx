0f0b2fd316e3024c59b592e7e1d87e4b39f7b51e
loaiza-devsoft <loaiza.devsoft@gmail.com>
2026-03-07 16:35:35 -0500

    Listar y eliminar categories menu hecho.

--- src/features/admin/categoryMenu/ui/listCategoryMenu/ListCategoryMenu.tsx
+++ src/features/admin/categoryMenu/ui/listCategoryMenu/ListCategoryMenu.tsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TableActions, TableBody, TableHead, TableLayout, TablePagination, Td, Th } from '../../../../../shared/components/componetsCrud/table/TableComponents';
 import "./listCategoryMenu.css"
+import ConfirmDeleteModal from '../../../../../shared/components/confirmDeleteModal/ConfirmDeleteModal';
+import { toast } from 'sonner';
+import { useDeleteEntity } from '../../../../../shared/hooks/useDeleteEntity';
+import { deleteCategoryMenu } from '../../../../../entities/categoryMenu/api/categoryMenuApi';
+import ButtonCasual from '../../../../../shared/components/buttonCasual/ButtonCasual';
+import { useListCategoryMenu } from '../../hooks/useListCategoryMenu';
 
 export default function ListCategoryMenu() {
-  return (
-    <div>ListCategoryMenu</div>
-  )
+
+    const {loading, error, categorysMenu, numberPage, totalPage, nextPage, prevPage, handleListCategoryMenu} = useListCategoryMenu();
+
+    // ---------- DELETE STATE ----------
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState<any>(null);
+
+    const { loading: deleting, remove } = useDeleteEntity(deleteCategoryMenu);
+
+    const openDeleteModal = (item: any) => {
+        setItemToDelete(item);
+        setShowDeleteModal(true);
+    };
+
+    const closeDeleteModal = () => {
+        setShowDeleteModal(false);
+        setItemToDelete(null);
+    };
+
+    const confirmDelete = async () => {
+        if (!itemToDelete) return;
+
+        await remove(itemToDelete.idMenuCategory);
+        closeDeleteModal();
+        handleListCategoryMenu();
+        toast.success("Categoria menu eliminado con exito.");
+    };
+
+    return (
+        <div className="listCategoryMenu__container-global">
+
+            {/* ---------- DELETE MODAL ---------- */}
+
+            {showDeleteModal && itemToDelete && (
+                <ConfirmDeleteModal
+                    title="Eliminar Categoria menu"
+                    description={`Estas a punto de eliminar permanentemente la Categoria menu con nombre "${itemToDelete.menuCategoryName}". Esta acción es irreversible.`}
+                    loading={deleting}
+                    onConfirm={confirmDelete}
+                    onClose={closeDeleteModal}
+                />
+            )}
+
+
+            <div className="listCategoryMenu__container-top">
+
+
+                <ButtonCasual linkRedireccion='/admin/category-menu-create' mensagge='+ Nueva adicion' />
+
+            </div>
+
+            <TableLayout>
+
+                <TableHead>
+                    <tr>
+                        <Th>ID</Th>
+                        <Th>NOMBRE</Th>
+                        <Th>DESCRIPCION</Th>
+                        <Th>ACCIONES</Th>
+                    </tr>
+                </TableHead>
+
+                <TableBody>
+                    {categorysMenu?.map((categoryMenu) => (
+                        <tr key={categoryMenu.idMenuCategory}>
+
+                            <Td>{categoryMenu.idMenuCategory}</Td>
+                            <Td>{categoryMenu.menuCategoryName}</Td>
+                            <Td>{categoryMenu.description}</Td>
+
+                            
+                            <Td>
+                                <TableActions
+                                    linkEdit={`/admin/category-menu/update/${categoryMenu.idMenuCategory}`}
+                                    onDelete={() => openDeleteModal(categoryMenu)}
+                                />
+                            </Td>
+
+                        </tr>
+                    ))}
+                </TableBody>
+
+            </TableLayout>
+
+            <TablePagination numberPage={numberPage} totalPage={totalPage} onNext={nextPage} onPrev={prevPage} />
+
+        </div>
+    )
 }
--- src/pages/admin/menuCategory/listMenuCategoryPage/ListMenuCategoryPage.tsx
+++ src/pages/admin/menuCategory/listMenuCategoryPage/ListMenuCategoryPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./listMenuCategoryPage.css"
 import AdminSidebar from '../../components/adminSidebar/AdminSidebar'
+import ListCategoryMenu from '../../../../features/admin/categoryMenu/ui/listCategoryMenu/ListCategoryMenu'
 
 export default function ListMenuCategoryPage() {
   return (
-    <div>
-      <AdminSidebar/>
+    <div className="listCategoryMenuPage__container-global">
+      <AdminSidebar />
+      <div className="listCategoryMenuPage__container-content">
+        <ListCategoryMenu />
+      </div>
     </div>
   )
 }
