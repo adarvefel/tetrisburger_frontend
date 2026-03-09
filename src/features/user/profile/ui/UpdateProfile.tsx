e45b7b823cf7531e178de74b4266963143d17569
loaiza-devsoft <loaiza.devsoft@gmail.com>
2026-03-09 17:49:21 -0500

    Se soluciono bug del perfil.

--- src/features/user/profile/ui/UpdateProfile.tsx
+++ src/features/user/profile/ui/UpdateProfile.tsx
@@ -4,11 +4,17 @@ import { useGetProfile } from '../hooks/useGetProfile'
 import { useUpdateProfile } from '../hooks/useUpdateProfile';
 import UserForm from '../../../../shared/components/formsCruds/userForm/UserForm';
 import LoadingSpinner from '../../../../shared/components/loadings/loadingSpinner/LoadingSpinner';
+import { useAuthStore } from '../../../../shared/store/useAuthStore';
+import { Link } from 'react-router-dom';
+import ButtonLogout from '../../../../shared/components/buttonLogout/ButtonLogout';
+import { GrUserAdmin } from "react-icons/gr";
 
 export default function UpdateProfile() {
 
     const { error: getError, loading: getLoading, profile, handleGetProfile } = useGetProfile();
     const { error: updateError, loading: updateLoading, handleUpdateProfile } = useUpdateProfile();
+
+    const { isAdmin } = useAuthStore();
 
     if (getLoading) {
         return <LoadingSpinner />
@@ -16,7 +22,14 @@ export default function UpdateProfile() {
 
     return (
         <div className="updateProfile__container-global">
-            <UserForm mode='user-update' onSubmit={handleUpdateProfile} initialData={profile ? profile : undefined} loading={updateLoading}/>
+            <UserForm mode='user-update' onSubmit={handleUpdateProfile} initialData={profile ? profile : undefined} loading={updateLoading} />
+            <div className="updateProfile__container-buttons">
+                <ButtonLogout />
+                {
+                    isAdmin ? <Link className='updateProfile__link' to={"/admin/users-list"}><GrUserAdmin size={27} /> Dashboard Administrador</Link> : null
+                }
+            </div>
         </div>
+
     )
 }
